f8b17fc04073f765e1306f0767ab0f033454da32
v-log <v-log1313@mail.ru>
2018-07-06 05:43:48 +0300

    Temp save on 06.07.18 07:43

--- src/data_preprocessor.rb
+++ src/data_preprocessor.rb
@@ -24,7 +24,33 @@ def process_match(player_id_col, curr_match_ind, matches)
     next if prev_match[player_id_col] != curr_match[player_id_col]
        || curr_match[oppo_col] != curr_match[player_id_col]
 
-    
-    
   end
+
+  # Headers for a new file
+  headers = matches.headers[0..30]
+  
+  # per = period (e.g. last 2 weeks),, df = double 
+  # faults, 1st_in = first serve in, %w_1st = % of 
+  # first serve won, %w_2nd = % of second serve won.
+  add_headers = ["1/rank_diff", "1/rank_p_diff", 
+		 "1/age_diff", "1/hgt_diff",
+                 "1/aces_last_diff", "1/aces_per_avg_diff",
+                 "1/aces_per_max_diff", "1/aces_per_min_diff",
+		 "1/df_last_diff", "1/df_per_avg_diff",
+		 "1/df_per_max_diff", "1/df_per_min_diff",
+		 "1/1st_in_last_diff", "1/1st_in_per_avg_diff",
+		 "1/1st_in_per_max_diff", "1/1st_in_per_min_diff",
+		 "1/%w_1st_last_diff", "1/%w_1st_per_avg_diff",
+		 "1/%w_1st_per_max_diff", "1/%w_1st_per_min_diff",
+		 "1/%w_2nd_last_diff", "1/%w_2nd_per_avg_diff",
+		 "1/%w_2nd_per_max_diff", "1/%w_2nd_per_min_diff"]
+
+  add_headers.each do |new_header|
+    headers << new_header
+  end
+
+  # Add headers to a new file
+  sorted_matches[0] = headers
+
+
 end

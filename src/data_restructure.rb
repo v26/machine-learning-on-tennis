e46e430aa1e8a5cd50f953f508e43c6fea82aacc
v-log <v-log1313@mail.ru>
2018-07-09 20:56:35 +0300

    clean code

--- src/data_restructure.rb
+++ src/data_restructure.rb
@@ -1,41 +1,30 @@
 require 'smarter_csv'
 
-def restructure_data
-  path = "../data/match_data_downloaded"
-  file = "atp_matches_1993.csv"
+def restructure_data(path, file)
+#  path = "../data/match_data_downloaded"
+#  file = "atp_matches_1993.csv"
   dir_sep = "/"
 
   # read entire csv
   matches = SmarterCSV.process("#{path}#{dir_sep}#{file}")
 
-  # sort data by tourney date, id, round 
-  # from latest to oldest one
   sort_matches!(matches)
 
-  print_m(matches)
+#  print_m(matches)
 
-  puts
-  check_rounds(matches)
-
-#  preprocessed_data = preprocess_data(matches)
+#  check_rounds(matches)
+  matches
 end
 
 private
 
 def sort_matches!(matches)
-  # Set of tourney rounds from final to first ones
+  # set of tourney rounds from final to first ones
   rounds = ["F", "SF", "QF", "R16", "R32", "R64", "R128", "RR"]
 
+  # sort data by tourney date, id, round
+  # from latest to oldest one
   matches.sort_by! { |match| [-(match[:tourney_date].to_i), match[:tourney_id], rounds.index(match[:round].to_s)] }
-end
-
-def preprocess_data(matches)
-  res = Array.new
-
-  (0...matches.size).each do |ind|
-    curr_match_data = Array.new   
-
-
 end
 
 def print_m(matches)
@@ -46,8 +35,9 @@ def print_m(matches)
 end
 
 def check_rounds(matches)
+  puts
   uniq_rounds = matches.uniq { |match| match[:round] }
   print_m(uniq_rounds)
 end
 
-restructure_data
+#restructure_data

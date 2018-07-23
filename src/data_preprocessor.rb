fcc17df3abd9191a524351b4ce8f5c26608e829a
v-log <v-log1313@mail.ru>
2018-07-23 13:49:58 +0300

    Implement #preprocess_matches

--- src/data_preprocessor.rb
+++ src/data_preprocessor.rb
@@ -1,133 +1,370 @@
 require_relative 'data_sort'
 require 'smarter_csv'
+require 'csv'
 require 'date'
-  
-def preprocess_data
-  path = '../data/match_data_downloaded'
-  file = 'atp_matches_2017.csv'
 
-  matches = sort_matches(path, file)
-#  print_m(matches)
-#  check_rounds(matches)
-  
-  preprocess_matches(matches)
-end
+module Data_Preprocessor
 
-private
-
-=begin
   # per = period (e.g. last 10 days), 
   # df = double faults,
   # 1st_in = first serve in,
   # %w_1st = % of first serve won,
   # %w_2nd = % of second serve won.
-  res_headers = ["1/rank_diff", "1/rank_p_diff", 
-		 "1/age_diff", "1/hgt_diff",
-                 "1/aces_last_diff", "1/aces_per_avg_diff",
-                 "1/aces_per_max_diff", "1/aces_per_min_diff",
-		 "1/df_last_diff", "1/df_per_avg_diff",
-		 "1/df_per_max_diff", "1/df_per_min_diff",
-		 "1/1st_in_last_diff", "1/1st_in_per_avg_diff",
-		 "1/1st_in_per_max_diff", "1/1st_in_per_min_diff",
-		 "1/%w_1st_last_diff", "1/%w_1st_per_avg_diff",
-		 "1/%w_1st_per_max_diff", "1/%w_1st_per_min_diff",
-		 "1/%w_2nd_last_diff", "1/%w_2nd_per_avg_diff",
-		 "1/%w_2nd_per_max_diff", "1/%w_2nd_per_min_diff" ]
+  @@res_headers = [
+    "1/rank_diff",           "1/rank_p_diff",
+    "1/age_diff",            "1/hgt_diff",
+    "1/aces_last_diff",      "1/aces_per_avg_diff",
+    "1/aces_per_max_diff",   "1/aces_per_min_diff",
+    "1/df_last_diff",        "1/df_per_avg_diff",
+    "1/df_per_max_diff",     "1/df_per_min_diff",
+    "1/1st_in_last_diff",    "1/1st_in_per_avg_diff",
+    "1/1st_in_per_max_diff", "1/1st_in_per_min_diff",
+    "1/%w_1st_last_diff",    "1/%w_1st_per_avg_diff",
+    "1/%w_1st_per_max_diff", "1/%w_1st_per_min_diff",
+    "1/%w_2nd_last_diff",    "1/%w_2nd_per_avg_diff",
+    "1/%w_2nd_per_max_diff", "1/%w_2nd_per_min_diff"
+  ]
 
-=end
+#  @@headers_for_processed = [
+#    "rank",           "rank_points",
+#    "age",            "ht",
+#    "aces_last",      "aces_per_avg",
+#    "aces_per_max",   "aces_per_min",
+#    "df_last",        "df_per_avg",
+#    "df_per_max",     "df_per_min",
+#    "1st_in_last",    "1st_in_per_avg",
+#    "1st_in_per_max", "1st_in_per_min",
+#    "%w_1st_last",    "%w_1st_per_avg",
+#    "%w_1st_per_max", "%w_1st_per_min",
+#    "%w_2nd_last",    "%w_2nd_per_avg",
+#    "%w_2nd_per_max", "%w_2nd_per_min"
+#  ]
 
-def preprocess_matches(matches)
-  res = []
-  # period for previous matches processing, days
-  per = 14
-  # number of previous matches to process
-  num = 10
+  @@headers_to_parse = [
+    :winner_rank,        :loser_rank,
+    :winner_rank_points, :loser_rank_points,
+    :winner_age,         :loser_age,
+    :winner_ht,          :loser_ht,
+    :w_ace,              :l_ace,
+    :w_df,               :l_df,
+    :w_svpt,             :l_svpt,
+    :w_1stin,            :l_1stin,
+    :w_1stwon,           :l_1stwon,
+    :w_2ndwon,           :l_2ndwon,
+  ]
 
-  c = 0
-  matches.each_with_index do |match, index|
-    winner_matches = get_valid_matches(matches, index, per, num, :winner_name)
-    next if winner_matches.nil?
-#    print_data(winner_matches, winner_matches.size)
-    loser_matches = get_valid_matches(matches, index, per, num, :loser_name)
-    next if loser_matches.nil?
+  def preprocess_data
+    path = '../data/match_data_downloaded'
+    file = 'atp_matches_2017.csv'
 
+    raw_matches = sort_matches(path, file)
 
-#    winner_processed = process_matches(winner_matches)
-#    loser_processed = process_matches(loser_matches)
-#    if index % 2 == 0 
-#      res = get_res(winner_processed, loser_processed, 1)
-#    else
-#      res = get_res(loser_processed, winner_processed, -1)
-#    end
-
-#    write_res(res)
-  c += 1
-  break if c == 3
+    preprocess_matches(raw_matches)
   end
-end
 
-def get_valid_matches(matches, index, per, num, player_name)
-  match_data = []
-  curr_match = matches[index]
-  curr_date = Date.strptime(curr_match[:tourney_date].to_s, '%Y%m%d')
-  curr_name = curr_match[player_name]
+  private
 
-  headers_to_process = [ :winner_rank, :loser_rank,
-			 :winner_rank_points, :loser_rank_points,
-			 :winner_age, :loser_age,
-			 :winner_ht, :loser_ht,
-			 :w_ace, :l_ace,
-			 :w_df, :l_df,
-			 :w_svpt, :l_svpt,
-			 :w_1stin, :l_1stin,
-			 :w_1stwon, :l_1stwon,
-			 :w_2ndwon, :l_2ndwon, ]
+  def preprocess_matches(matches)
+    output_name = "test.csv"
+    # period for previous matches processing, days
+    per = 14
+    # number of previous matches to process
+    num_min = 3
+    num_max = 10
 
-  matches[(index + 1)...matches.size].each do |prev_match|
-    catch :missing_value do
-      prev_date = Date.strptime(prev_match[:tourney_date].to_s, '%Y%m%d')
+    headers_written = false
 
-      valid_date = (curr_date - prev_date).to_i < per
-      valid_count = match_data.size < num
-      valid_name = curr_name == prev_match[:winner_name] ||
-                   curr_name == prev_match[:loser_name]
+    c = 0
+    matches.each_with_index do |match, index|
+      winner_matches = get_valid_matches(
+        matches,
+        index,
+        per,
+        num_min,
+        num_max,
+        :winner_name
+      )
+      next if winner_matches.nil?
 
-      if valid_date && valid_count
-        if valid_name
-          add_match = {}
+      loser_matches = get_valid_matches(
+        matches,
+        index,
+        per,
+        num_min,
+        num_max,
+        :loser_name
+      )
+      next if loser_matches.nil?
 
-          if curr_name == prev_match[:winner_name]
-            headers = headers_to_process.select.with_index { |_, i| i.even? }
-          else
-            headers = headers_to_process.select.with_index { |_, i| i.odd? }
-          end
-
-          headers.each do |header|
-            throw :missing_value and return nil if prev_match[header].nil?
-
-            add_match[header] = prev_match[header]
-          end
-          match_data << add_match
-        end
+#puts "winner:"
+#puts winner_matches.inspect
+#puts
+#puts "loser:"
+#puts loser_matches.inspect
+#puts
+      winner_processed = process_matches(winner_matches)
+#puts
+      loser_processed = process_matches(loser_matches)
+#puts
+#puts "!!!"
+#puts
+      if index % 2 == 0 
+        res = get_res(winner_processed, loser_processed, 1)
       else
-        break
+        res = get_res(loser_processed, winner_processed, -1)
+      end
+
+      if !headers_written
+        CSV.open(output_name, 'wb') do |csv|
+          csv << res.keys
+        end
+        headers_written = true
+      end
+
+puts res.inspect
+      write_res(res, output_name)
+      c += 1
+      break if c == 11
+    end
+  end
+
+  def get_valid_matches(matches, index, per, num_min, num_max, player_name)
+    match_data = []
+    curr_match = matches[index]
+    curr_date = get_date(curr_match[:tourney_date], '%Y%m%d')
+    curr_name = curr_match[player_name]
+
+    matches[(index + 1)...matches.size].each do |prev_match|
+      catch :missing_value do
+        prev_date = get_date(prev_match[:tourney_date], '%Y%m%d')
+
+        valid_date = (curr_date - prev_date).to_i < per
+        valid_count = match_data.size <= num_max
+        valid_name = curr_name == prev_match[:winner_name] ||
+                     curr_name == prev_match[:loser_name]
+
+        if valid_date && valid_count
+          if valid_name
+            add_match = {}
+
+            if curr_name == prev_match[:winner_name]
+              headers = @@headers_to_parse.select.with_index { |_, i| i.even? }
+            else
+              headers = @@headers_to_parse.select.with_index { |_, i| i.odd? }
+            end
+
+            headers.each do |header|
+              throw :missing_value and return nil if prev_match[header].nil?
+              add_match[header] = prev_match[header]
+            end
+            match_data << add_match
+          end
+        else
+          break
+        end
       end
     end
+    match_data.size < num_min ? nil : match_data
   end
-#  print_data(match_data, match_data.size, *headers_to_process)
-  match_data
-end
 
-def print_data(data_set, size, *attributes)
-  size = data_set.size if size > data_set.size
-  (0...size).each do |i|
-    item = data_set[i]
-    attributes = item.keys if attributes.empty?
-    attributes.each do |key|
-      print "#{item[key]}  "
+  def process_matches(matches)
+#  @@headers_for_processed = [
+#    "rank",            "rank_points",
+#    "age",             "ht",
+#    "aces_last",       "aces_per_avg",
+#    "aces_per_max",    "aces_per_min",
+#    "df_last",         "df_per_avg",
+#    "df_per_max",      "df_per_min",
+#    "perc_1st_in_last",    "perc_1st_in_per_avg",
+#    "perc_1st_in_per_max", "perc_1st_in_per_min",
+#    "perc_w_1st_last",     "perc_w_1st_per_avg",
+#    "perc_w_1st_per_max",  "perc_w_1st_per_min",
+#    "perc_w_2nd_last",    "perc_w_2nd_per_avg",
+#    "perc_w_2nd_per_max", "perc_w_2nd_per_min"
+#  ]
+
+#  @@headers_to_parse = [
+#    :winner_rank,        :loser_rank,
+#    :winner_rank_points, :loser_rank_points,
+#    :winner_age,         :loser_age,
+#    :winner_ht,          :loser_ht,
+#    :w_ace,              :l_ace,
+#    :w_df,               :l_df,
+#    :w_svpt,             :l_svpt,
+#    :w_1stin,            :l_1stin,
+#    :w_1stwon,           :l_1stwon,
+#    :w_2ndwon,           :l_2ndwon,
+#  ]
+
+    res = {}
+
+    prefix = ""
+
+    headers_2_dup = [:rank, :rank_points, :age, :ht]
+    headers_2_dup.each do |header|
+      prefix = matches[0][:winner_rank].nil? ? "loser_" : "winner_"
+      header_2_process = concat_sym(prefix, header)
+      result = matches[0][header_2_process]
+#      result = last(matches, header.to_s)
+      if result.class.name == "Float"
+        res[header] = result.round(3)
+      else
+        res[header] = result
+      end
     end
-  puts if size > 0
+
+    res[:aces_last] = last(matches, "ace")
+    res[:aces_per_avg] = avg(matches, "ace")
+    res[:aces_per_min] = min(matches, "ace")
+    res[:aces_per_max] = max(matches, "ace")
+
+    res[:df_last] = last(matches, "df")
+    res[:df_per_avg] = avg(matches, "df")
+    res[:df_per_min] = min(matches, "df")
+    res[:df_per_max] = max(matches, "df")
+
+    res[:perc_1st_in_last] = last(matches, "1stin", "svpt") { |a, b| perc(a, b) }
+    res[:perc_1st_in_avg] = avg(matches, "1stin", "svpt") { |a, b| perc(a, b) }
+    res[:perc_1st_in_min] = min(matches, "1stin", "svpt") { |a, b| perc(a, b) }
+    res[:perc_1st_in_max] = max(matches, "1stin", "svpt") { |a, b| perc(a, b) }
+
+    res[:perc_w_1st_last] = last(matches, "1stwon", "1stin") { |a, b| perc(a, b) }
+    res[:perc_w_1st_avg] = avg(matches, "1stwon", "1stin") { |a, b| perc(a, b) }
+    res[:perc_w_1st_min] = min(matches, "1stwon", "1stin") { |a, b| perc(a, b) }
+    res[:perc_w_1st_max] = max(matches, "1stwon", "1stin") { |a, b| perc(a, b) }
+
+    res[:perc_w_2nd_last] = last(matches, "2ndwon", "1stin", "svpt") { |a, b, c| perc(a, c - b)}
+    res[:perc_w_2nd_avg] = avg(matches, "2ndwon", "1stin", "svpt") { |a, b, c| perc(a, c - b)}
+    res[:perc_w_2nd_min] = min(matches, "2ndwon", "1stin", "svpt") { |a, b, c| perc(a, c - b)}
+    res[:perc_w_2nd_max] = max(matches, "2ndwon", "1stin", "svpt") { |a, b, c| perc(a, c - b)}
+
+#puts res.inspect
+    res
+  end
+
+  def concat_sym(prefix, appen)
+    sym = (prefix.to_s + appen.to_s).to_sym
+  end
+
+  def perc(part, full)
+    perc = part.to_f * 100 / full.to_f
+    perc.round(2)
+  end
+
+  def last(matches, *attribs)
+#    get_var = block_given? ? lambda { |item| yield(item) } : lambda { |item| item }
+    last_match = matches[0]
+    full_attribs = attribs.map { |attr| attr = last_match.keys.find { |key| /[\w\d]*#{attr}/ =~ key.to_s }.to_sym }
+    if block_given?
+      args = []
+      full_attribs.each { |attr| args << last_match[attr] }
+      last = yield(*args)
+#    last = get_var[symb]
+    else
+      last = last_match[full_attribs[0]]
+    end
+#puts "test last: #{attribs} = #{last}"
+    last
+  end
+
+  def avg(matches, *attribs)
+    avg = 0
+    args = []
+    sum = 0
+    matches.each do |match|
+      full_attribs = attribs.map { |attr| attr = match.keys.find { |key| /[\w\d]*#{attr}/ =~ key.to_s }.to_sym }
+      args = full_attribs.map { |attr| match[attr]}
+      if block_given?
+        sum += yield(*args)
+      else
+        sum += args[0]
+      end
+    end
+    avg = (sum / matches.size).round(2)
+#      last = get_var[symb]
+#puts "test avg: #{attribs} = #{avg}"
+    avg
+  end
+
+  def min(matches, *attribs)
+    args = []
+    res = []
+    matches.each do |match|
+      full_attribs = attribs.map { |attr| attr = match.keys.find { |key| /[\w\d]*#{attr}/ =~ key.to_s }.to_sym }
+      args = full_attribs.map { |attr| match[attr]}
+      if block_given?
+        res << yield(*args)
+      else
+        res << args[0]
+      end
+    end
+    min = res.min.round(2)
+#puts "test min: #{attribs} = #{min}"
+    min
+  end
+
+  def max(matches, *attribs)
+    args = []
+    res = []
+    matches.each do |match|
+      full_attribs = attribs.map { |attr| attr = match.keys.find { |key| /[\w\d]*#{attr}/ =~ key.to_s }.to_sym }
+      args = full_attribs.map { |attr| match[attr]}
+      if block_given?
+        res << yield(*args)
+      else
+        res << args[0]
+      end
+    end
+    max = res.max.round(2)
+#puts "test max: #{attribs} = #{max}"
+    max
+  end
+
+  def get_res(p1_data, p2_data, res_flag)
+    res = {}
+    p1_data.keys.each do |key|
+      key_str = "1/" + key.to_s + "_diff"
+#      res_key = "1/#{key.to_s}_diff".to_sym
+      res_key = key_str.to_sym
+      p1_val = p1_data[key].to_i
+      p2_val = p2_data[key].to_i
+      diff = p1_val - p2_val
+      
+      res[res_key] = diff == 0 ? 0 : (1 / diff.to_f).round(4)
+#puts "#{res_key} = #{res[res_key]}" if res[res_key] < -1 || res[res_key] > 1
+    end
+    res[:res] = res_flag
+#puts "res:"
+#puts res.inspect
+    res
+  end
+
+  def write_res(res, file_name)
+    CSV.open(file_name, 'a+') do |csv|
+      csv << res.values.to_a
+    end
+  end
+
+  def print_data(data_set, size, *attributes)
+    size = data_set.size if size > data_set.size
+    puts data_set.inspect
+    puts size
+    puts attributes.inspect
+    (0...size).each do |i|
+      item = data_set[i]
+      attrib = item.keys if attributes.empty?
+      puts attrib.inspect
+      attrib.each do |key|
+        print "#{item[key]}  "
+      end
+    puts if size > 0
+    end
+  end
+
+  def get_date(match, format)
+    Date.strptime(match.to_s, format)
   end
 end
 
+include Data_Preprocessor
 preprocess_data
